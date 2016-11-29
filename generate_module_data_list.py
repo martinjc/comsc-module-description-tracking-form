a9c5777289523d52ca9aedb1402eabed1ff3bfdf
Martin Chorley <me@martinjc.com>
2016-11-29 16:57:08 +0000

    added comsc module leader

--- generate_module_data_list.py
+++ generate_module_data_list.py
@@ -14,7 +14,7 @@ with open(os.path.join(SRC_DATA_DIR, 'module_data.csv'), 'r') as in_file:
 
     csv_reader = csv.DictReader(in_file)
     for row in csv_reader:
-        modules.append({'module_code': row['Code'], 'module_title': row['Title']})
+        modules.append({'module_code': row['Code'], 'module_title': row['Title'], 'module_leader_(comsc-data)': row['Staff']})
 
 with open(os.path.join(SRC_DATA_DIR, 'module_data_list.json'), 'w') as out_file:
     json.dump(modules, out_file)
