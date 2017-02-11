8ce26afd4b4678d5a0ede4a16441182bc2a0b252
Martin Chorley <me@martinjc.com>
2017-02-11 08:45:15 +0000

    updated merging diffs to pdf

--- combine_pdf_diffs.py
+++ combine_pdf_diffs.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 
     diff_folder = os.path.join(os.getcwd(), 'comparisons', 'diffs')
 
-    for f in ['year1', 'year2', 'year3', 'msc', 'nsa_year1', 'nsa_year2', 'nsa_year3']:
+    for f in ['year1', 'year2', 'year3', 'msc', 'nsa_year1', 'nsa_year2', 'nsa_year3', 'msc_cmp', 'msc_cit', 'msc_dysa', 'msc_cdj', 'msc_isp', 'msc_acs']:
 
         if not os.path.exists(os.path.join(diff_folder, '%s_diff.html' % f)):
             open(os.path.join(diff_folder, '%s_diff.html' % f), 'w')
@@ -31,6 +31,18 @@ if __name__ == "__main__":
                 add_diff(os.path.join(diff_folder, mcode, '%s_diff.html' % mcode), os.path.join(diff_folder, 'year3_diff.html'))
             elif mcode in MSC:
                 add_diff(os.path.join(diff_folder, mcode, '%s_diff.html' % mcode), os.path.join(diff_folder, 'msc_diff.html'))
+                if mcode in PFMSADSA:
+                    add_diff(os.path.join(diff_folder, mcode, '%s_diff.html' % mcode), os.path.join(diff_folder, 'msc_acs_diff.html'))
+                if mcode in PFMSCITA:
+                    add_diff(os.path.join(diff_folder, mcode, '%s_diff.html' % mcode), os.path.join(diff_folder, 'msc_cit_diff.html'))
+                if mcode in PFMSCMPA:
+                    add_diff(os.path.join(diff_folder, mcode, '%s_diff.html' % mcode), os.path.join(diff_folder, 'msc_cmp_diff.html'))
+                if mcode in PFMSDSYA:
+                    add_diff(os.path.join(diff_folder, mcode, '%s_diff.html' % mcode), os.path.join(diff_folder, 'msc_dysa_diff.html'))
+                if mcode in PFMSISPA:
+                    add_diff(os.path.join(diff_folder, mcode, '%s_diff.html' % mcode), os.path.join(diff_folder, 'msc_isp_diff.html'))
+                if mcode in PFMSCDJ:
+                    add_diff(os.path.join(diff_folder, mcode, '%s_diff.html' % mcode), os.path.join(diff_folder, 'msc_cdj_diff.html'))
             elif mcode in NSA_YEAR1:
                 add_diff(os.path.join(diff_folder, mcode, '%s_diff.html' % mcode), os.path.join(diff_folder, 'nsa_year1_diff.html'))
             elif mcode in NSA_YEAR2:
