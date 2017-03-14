e0c4c44fc5bddcbe2f7ef79badf08fbaddbd2c39
Martin Chorley <me@martinjc.com>
2017-03-14 13:15:22 +0000

    updated fields and names for extraction

--- lib/handbookdata.py
+++ lib/handbookdata.py
@@ -6,6 +6,37 @@ from datetime import datetime
 
 FIELD_NAMES = ["module_name","module_code","level","delivery_language","module_leader","school_code","external_subject_code","number_of_credits","module_description","semester","learning_outcomes","delivery","skills","assessment","1_assessment_title","1_assessment_type","1_assessment_contribution","1_assessment_date","2_assessment_title","2_assessment_type","2_assessment_contribution","2_assessment_date","3_assessment_title","3_assessment_type","3_assessment_contribution","3_assessment_date","4_assessment_title","4_assessment_type","4_assessment_contribution","4_assessment_date","5_assessment_title","5_assessment_type","5_assessment_contribution","5_assessment_date","6_assessment_title","6_assessment_type","6_assessment_contribution","6_assessment_date","syllabus_content","essential_reading","background_reading"]
 
+NON_ASSESSMENT_FIELD_NAMES = [
+    "module_name","module_code","level","delivery_language","module_leader","school_code","external_subject_code","number_of_credits","module_description","semester","learning_outcomes","delivery","skills","assessment","syllabus_content","essential_reading","background_reading"
+]
+
+FIELD_NAMES_AND_LABELS = {
+    "module_name": "Module Name",
+    "module_code": "Module Code",
+    "level": "Level",
+    "delivery_language": "Language of Delivery",
+    "module_leader": "Module Leader",
+    "school_code": "School Code",
+    "external_subject_code": "External Subject Code",
+    "number_of_credits": "Number of Credits",
+    "module_description": "Outline Description of Module",
+    "semester": "Semester",
+    "learning_outcomes": "Learning Outcomes - On completion of the module a student should be able to",
+    "delivery": "How the module will be delivered",
+    "skills": "Skills that will be practiced and developed",
+    "assessment": "How the module will be assessed",
+    "syllabus_content": "Syllabus Content",
+    "essential_reading": "Essential Reading and Resource List",
+    "background_reading": "Background Reading and Resource List"
+}
+
+ASSESSMENT_NAMES_AND_LABELS = {
+    "_assessment_type": "Type",
+    "_assessment_contribution": "Contribution (%)",
+    "_assessment_title": "Title",
+    "_assessment_date": "Week"
+}
+
 YEAR1 = ['CM1101', 'CM1102', 'CM1103', 'CM1104', 'CM1201', 'CM1202', 'CM1204', 'CM1205', 'CM1206', 'CM1208', 'CM1209']
 YEAR2 = ['CM2101', 'CM2102', 'CM2103', 'CM2104', 'CM2105', 'CM2201', 'CM2203', 'CM2205', 'CM2206', 'CM2207', 'CM2208', 'CM2302', 'CM2303', 'CM2305', 'CM2500']
 YEAR3 = ['CM3101', 'CM3103', 'CM3104', 'CM3105', 'CM3106', 'CM3107', 'CM3109', 'CM3110', 'CM3111', 'CM3112', 'CM3113', 'CM3114', 'CM3201', 'CM3202', 'CM3203', 'CM3301', 'CM3302', 'CM3303', 'CM3304']
